8e71c97824caf4f86684485c48d6c0ef83a5294d
shiva508 <dasarishiva1@gmail.com>
2021-09-17 20:11:37 +0530

    initial

---
+++ src/main/java/com/pool/SpringDataZeroToHeroApplication.java
@@ -0,0 +1,13 @@
+package com.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataZeroToHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataZeroToHeroApplication.class, args);
+	}
+
+}
